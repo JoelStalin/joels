6afb8f61466a24ddc0ebef6016c8ce5e95f33f7d
JoelStalin <ing.joelstalinmartinez@gmail.com>
2017-05-25 10:18:17 -0400

    --all

--- server.js
+++
@@ -1,8 +0,0 @@
-const app = require('./config/express')();
- app.get('/', function (req, res) {
-  app.send('home',{index});
-});
-
-app.listen(app.get('port'), function(){
-  console.log('App listening at port ' + app.get('port'));
-});
